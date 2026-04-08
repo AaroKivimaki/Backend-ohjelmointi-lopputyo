aaf6c686b6365f30d50e34464f833695a48e1a3a
moikill231 <aaro.kivimaki@gmail.com>
2026-04-08 16:59:33 +0300

    Tehty asioita

---
+++ db/models.py
@@ -0,0 +1,29 @@
+from sqlmodel import SQLModel, Field
+
+
+class PelaajaBase(SQLModel):
+    name: str
+    events: [str]
+
+
+class PelaajaIn(PelaajaBase):
+    pass
+
+
+class PelaajaDb(PelaajaBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+
+
+class EventBase(SQLModel):
+    type: [str]
+    detail: str
+    player_id: int
+    timestamp: str
+
+
+class EventDb(EventBase, table=True):
+    player_id: int = Field(default=None, primary_key=True)
+
+
+class EventIn(EventBase):
+    pass
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+from .db.database import create_db_and_tables
 
-app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield create_db_and_tables()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
